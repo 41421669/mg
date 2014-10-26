ee12b920c638d35e02689f65214b348f3e98634f
lj <597387976@qq.com>
2014-10-26 19:29:34 +0800

    delete

--- settings.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	cookieSecret: 'icsser',
-	db_name: 'EquuFIqxPbKLKpdhsSpT',
-	db_table: 'db_icsser',
-	db_host: 'mongo.duapp.com',
-	db_port: '8908',
-	username: 'WDoXb1LUjfCZVTPg4mROIizn',
-	password: 'ZSAqBllra7n4LeCVjbpsGg0i0cyaDGdU'
-}
